a8cb99403b7d50df17132815bdb95c265a9f0cfc
Raqibur-Rahman <raqibur.edu@gmail.com>
2023-06-02 20:32:09 +0600

    AOS added in homepage

--- src/pages/Home/Home/Banner/Banner.jsx
+++ src/pages/Home/Home/Banner/Banner.jsx
@@ -2,6 +2,7 @@ import './Banner.css'
 import picture1 from '../../../../assets/images/BannerPicture1.jpg';
 import picture2 from '../../../../assets/images/BannerPicture2.jpg';
 import picture3 from '../../../../assets/images/BannerPicture3.jpg';
+import { Link } from 'react-router-dom';
 const Banner = () => {
     return (
         <div className="banner-container">
@@ -9,7 +10,7 @@ const Banner = () => {
                 <div id="slide1" className="carousel-item relative w-full">
                     <img src={picture1} className="w-full rounded-lg" />
                     <div className="absolute h-full flex items-center justify-between   bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-                        <div className='lg:space-y-7 lg:w-1/3 p-1 pl-12'>
+                        <div className='lg:space-y-7 lg:w-1/3 p-1 pl-12' data-aos="zoom-in">
                             <h3 className='lg:text-3xl text-pink-700 font-bold lg:m-1 lg:p-1'>
                                 Ignite Curiosity, Fuel Learning.
                             </h3>
@@ -18,7 +19,10 @@ const Banner = () => {
                                 <small className=''> Welcome to intellectoToys, where learning becomes an adventure! Explore our wide range of educational toys, designed to unlock young minds and inspire curiosity.</small>
                             </p>
                             <p className='text-pink-300 lg:font-semibold'>Limited Time Offer: Get 20% Off All Science Kits!</p>
-                            <button className="btn  btn-success lg:font-bold">Explore Now</button>
+                            <button className="btn  btn-success lg:font-bold">
+                                <Link to="/alltoys">Explore Now</Link>
+                            </button>
+
                         </div>
                     </div>
                     <div className="absolute flex justify-center transform -translate-y-1/2 left-5 right-5 bottom-0">
@@ -32,10 +36,10 @@ const Banner = () => {
                     <div className="absolute h-full flex items-center justify-between   bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
                         <div className='lg:space-y-7 lg:w-1/3 p-1 pl-12'>
                             <h3 className='lg:text-3xl text-pink-700 font-bold lg:m-1 lg:p-1'>
-                            Unlock the Power of Learning!
+                                Unlock the Power of Learning!
                             </h3>
                             <p className='lg:text-xl text-blue-300  lg:m-1 lg:p-1 '>
-                            Discover the Joy of Educational Play! <br />
+                                Discover the Joy of Educational Play! <br />
                                 <small className=''> We believe in the power of play to ignite curiosity and empower young minds. Discover our carefully selected educational toys that foster critical thinking, problem-solving skills, and a love for learning.</small>
                             </p>
                             <p className='text-pink-300 lg:font-semibold'>Math Mania Sale: Buy One, Get One 50% Off!</p>
@@ -54,10 +58,10 @@ const Banner = () => {
                     <div className="absolute h-full flex items-center justify-between   bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
                         <div className='lg:space-y-7 lg:w-1/3 p-1 pl-12'>
                             <h3 className='lg:text-3xl text-pink-700 font-bold lg:m-1 lg:p-1'>
-                            Build the Innovators of Tomorrow!
+                                Build the Innovators of Tomorrow!
                             </h3>
                             <p className='lg:text-xl text-blue-300  lg:m-1 lg:p-1 '>
-                            Inspire, Create, Engineer! <br />
+                                Inspire, Create, Engineer! <br />
                                 <small className=''> Our engineering kits and tools are designed to inspire creativity, critical thinking, and a passion for innovation. From building intricate models to experimenting with real engineering principles, our toys encourage hands-on learning. </small>
                             </p>
                             <p className='text-pink-300 lg:font-semibold'>Free Tool Set with Engineering Kits!</p>
--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,33 +1,36 @@
 
-import logo from '../../../assets/images/logo.png'
+
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/images/logo.png';
+
 const Footer = () => {
     return (
         <div>
             <footer className="footer p-10 bg-base-200 text-base-content">
                 <div>
                     <span className="footer-title">Services</span>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
+                    <Link to="/" className="link link-hover">Branding</Link>
+                    <Link to="/" className="link link-hover">Design</Link>
+                    <Link to="/" className="link link-hover">Marketing</Link>
+                    <Link to="/" className="link link-hover">Advertisement</Link>
                 </div>
                 <div>
                     <span className="footer-title">Company</span>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
+                    <Link to="/about" className="link link-hover">About us</Link>
+                    <Link to="/contact" className="link link-hover">Contact</Link>
+                    <Link to="/jobs" className="link link-hover">Jobs</Link>
+                    <Link to="/press" className="link link-hover">Press kit</Link>
                 </div>
                 <div>
                     <span className="footer-title">Legal</span>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
+                    <Link to="/terms" className="link link-hover">Terms of use</Link>
+                    <Link to="/privacy" className="link link-hover">Privacy policy</Link>
+                    <Link to="/cookie" className="link link-hover">Cookie policy</Link>
                 </div>
             </footer>
             <footer className="footer px-10 py-4 border-t bg-base-200 text-base-content border-base-300">
                 <div className="items-center grid-flow-col">
-                   <img src={logo} alt="" />
+                    <img src={logo} alt="" />
                     <p>IntellectoToys <br />Providing reliable tech since 1992</p>
                 </div>
                 <div className="md:place-self-center md:justify-self-end">
@@ -38,7 +41,6 @@ const Footer = () => {
                     </div>
                 </div>
             </footer>
-
         </div>
     );
 };
