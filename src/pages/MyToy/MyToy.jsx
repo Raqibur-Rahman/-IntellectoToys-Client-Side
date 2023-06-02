463364f6837bdd005d76f484787b8af3c17c1821
Raqibur-Rahman <raqibur.edu@gmail.com>
2023-06-02 09:10:35 +0600

    Delete and Update functionality added

--- src/pages/MyToy/MyToy.jsx
+++ src/pages/MyToy/MyToy.jsx
@@ -1,19 +1,83 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import MyToyRow from "./MyToyRow";
+import Swal from 'sweetalert2';
+
 
 const MyToy = () => {
     const { user } = useContext(AuthContext);
     const [myToys, setMyToys] = useState([]);
 
     const url = `http://localhost:5000/myToys?seller_email=${user?.email}`;
-    // console.log(user);
 
     useEffect(() => {
         fetch(url)
             .then(res => res.json())
             .then(data => setMyToys(data))
     }, [])
+
+    const handleDelete = (id) => {
+        const proceed = confirm("Are you sure you want to delete the item?");
+        if (proceed) {
+            fetch(`http://localhost:5000/toys/${id}`, {
+                method: "DELETE"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deleteCount > 0) {
+                        alert('Item has been deleted!');
+                    }
+                    setMyToys(prevToys => prevToys.filter(myToy => myToy._id !== id));
+                })
+        }
+    }
+
+    
+
+    const handleEdit = (id) => {
+        const updatedPrice = prompt("Enter the updated price:");
+        const updatedQuantity = prompt("Enter the updated available quantity:");
+        const updatedDescription = prompt("Enter the updated detail description:");
+
+        const updatedToy = {
+            price: updatedPrice,
+            quantity: updatedQuantity,
+            description: updatedDescription,
+        };
+
+        fetch(`http://localhost:5000/toys/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(updatedToy),
+        })
+            .then(() => {
+                setMyToys(prevToys => {
+                    const updatedToys = prevToys.map(myToy => {
+                        if (myToy._id === id) {
+                            return { ...myToy, ...updatedToy };
+                        }
+                        return myToy;
+                    });
+                    return updatedToys;
+                });
+
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: 'Your work has been saved',
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
+            .catch((error) => {
+                console.error("Error updating toy:", error);
+            });
+    };
+
+
     return (
         <div>
             <h3 className="text-3xl font-bold text-center">Your Toy</h3>
@@ -22,43 +86,30 @@ const MyToy = () => {
             <div>
                 <div className="overflow-x-auto">
                     <table className="table">
-
                         <thead>
                             <tr className="text-center">
-                               
                                 <th>Name</th>
                                 <th>Price</th>
-                                <th>Available Quantity </th>
+                                <th>Available Quantity</th>
                                 <th>Detail description</th>
                                 <th>Actions</th>
                                 <th>Favorite Color</th>
                                 <th></th>
-
-                                {/* "Seller": "Raqibur Rahman",
-        "seller_email": "raqibur.a@gmail.com" */}
                             </tr>
                         </thead>
                         <tbody>
-
-                            {
-                                myToys.map(myToy =>
-                                    <MyToyRow
-                                        key={myToy._id}
-                                        myToy={myToy}
-                                    ></MyToyRow>)
-                            }
-
-
-
-
-
+                            {myToys.map(myToy =>
+                                <MyToyRow
+                                    key={myToy._id}
+                                    myToy={myToy}
+                                    handleDelete={handleDelete}
+                                    handleEdit={handleEdit}
+                                />
+                            )}
                         </tbody>
-
-
                     </table>
                 </div>
             </div>
-
         </div>
     );
 };
--- src/pages/MyToy/MyToyRow.jsx
+++ src/pages/MyToy/MyToyRow.jsx
@@ -1,27 +1,16 @@
 import { useState } from 'react';
 
 
-const MyToyRow = ({ myToy }) => {
-    const { picture, name, price, brand, quantity, description, category, summary, Seller, seller_email } = myToy;
+
+const MyToyRow = ({ myToy, handleDelete, handleEdit }) => {
+    const { _id, picture, name, price, brand, quantity } = myToy;
     const [isDeleteHovered, setIsDeleteHovered] = useState(false);
-    const [isEditHovered, setIsEditHovered] = useState(false);
+    // const [isEditHovered, setIsEditHovered] = useState(false);
 
     const handleDeleteHover = () => {
         setIsDeleteHovered(!isDeleteHovered);
     };
 
-   const handleDelete=id=>{
-
-    const proceed= confirm('Are you really want to DELETE?');
-    if(proceed){
-        fetch('')
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data);
-        })
-    }
-
-   }
 
 
     return (
@@ -43,19 +32,21 @@ const MyToyRow = ({ myToy }) => {
             <td>{quantity}</td>
             <td>
                 <div>
-                    {/* <p>{description}</p> */}
+                    <p>{_id}</p>
                 </div>
             </td>
             <td>
                 <div className="flex text-center">
                     <div className="p-3 text-center">
-                        <button className="btn btn-sm btn-circle btn-warning" data-tip="Delete" onMouseEnter={handleDeleteHover} onMouseLeave={handleDeleteHover}  onClick={ handleDelete}>
+                        <button className="btn btn-sm btn-circle btn-warning" data-tip="Delete" onMouseEnter={handleDeleteHover} onMouseLeave={handleDeleteHover} onClick={() => handleDelete(_id)}>
                             <img src="https://cdn-icons-png.flaticon.com/512/399/399274.png?w=740&t=st=1685622220~exp=1685622820~hmac=5594f69811603c4ac1937784c5161d573b3f8a6eab80a9dac400f896ff69b155" alt="" />
                             {isDeleteHovered && <span className="tooltip-text">Delete</span>}
                         </button>
+
+
                     </div>
                     <div className="p-3 text-center">
-                        <button className="btn btn-sm btn-circle btn-warning" data-tip="Delete" onMouseEnter={handleDeleteHover} onMouseLeave={handleDeleteHover}>
+                        <button className="btn btn-sm btn-circle btn-warning" data-tip="Delete" onMouseEnter={handleDeleteHover} onMouseLeave={handleDeleteHover} onClick={() => handleEdit(_id)}>
                             <img src="https://cdn-icons-png.flaticon.com/512/84/84380.png?w=740&t=st=1685622334~exp=1685622934~hmac=bd16a4bda463383a7533650800ff5525b70f8daa2914985caa6c35601d62c00d" alt="" />
                             {isDeleteHovered && <span className="tooltip-text">Edit</span>}
                         </button>
